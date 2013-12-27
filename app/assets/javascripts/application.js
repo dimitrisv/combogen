c9b374570892d33a4e7b1ca123ed5b38b30a70f9
DimitrisVainas <vainas.dim@gmail.com>
2013-12-27 23:18:52 +0200

    Watch tutorial functionality on trick#show page. Dynamically loading parsed video id from backend.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,44 @@ $(document).ready(function(){
     e.preventDefault();
   });
 
+  // make all elements with the .btn act as buttons
   $(".btn").button();
 
-});
+  // BOOTSTRAP 3.0 - Open YouTube Video Dynamicaly in Modal Window
+  // Modal Window for dynamically opening videos
+  // http://www.joshuawinn.com/opening-youtube-links-dynamically-in-a-twitter-bootstrap-modal-window/
+  $('a[data-video-id]').on('click', function(e){
+    // do not open modal until iframe is injected
+    e.preventDefault();
 
+    // retrieve relevant data attributes
+    var targetElement = $(this).attr('data-target');
+    var videoId = $(this).attr('data-video-id');
+
+    // check if they are legit
+    if ( !videoId ) {
+      var iFrameCode = "<h3>The data-video-id attribute was not specified.</h3>";
+    }
+
+    // Variables for iFrame code. Width and height from data attributes, else use default.
+    var vidWidth = 100; // default (in %)
+    var vidHeight = 315; // default (in px)
+
+    // create iframe code
+    var iFrameCode = '<iframe width="' + vidWidth + '%" height="'+ vidHeight +'" scrolling="no" allowtransparency="true" allowfullscreen="true" src="http://www.youtube.com/embed/'+ videoId +'?autoplay=1&rel=0&wmode=transparent&showinfo=0" frameborder="0"></iframe>';
+
+    // Replace modal's HTML with iFrame embed and open it
+    $(targetElement+' .modal-body .video-container').html(iFrameCode);
+    $(targetElement).modal();
+  });
+
+  // When modal closes, stop video playback.
+  $('#watch-tutorial-modal').on('hidden.bs.modal', function (e) {
+    $("#watch-tutorial-modal iframe").attr("src", "");
+  });
+  $('#watch-combo-modal').on('hidden.bs.modal', function (e) {
+    // refresh the 'src' attribute of the iframe so the video is forced to reload
+    $("#watch-combo-modal iframe").attr("src", $("#watch-combo-modal iframe").attr("src"));
+  });
+
+});
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,17 @@
 class Video < ActiveRecord::Base
   attr_accessible :url
+
+  # Currently assumes youtube video
+  def get_video_id
+    if url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    # later I can insert this directly as a data attribute
+    # %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  end
+
 end
