e5ace9e8aeae197efb54ca417793dc5430b89eec
DimitrisVainas <vainas.dim@gmail.com>
2013-12-31 14:32:45 +0200

    Add scroller to trick#show combos, minor bugfixes

--- app/controllers/tricks_controller.rb
+++ app/controllers/tricks_controller.rb
@@ -198,7 +198,7 @@ class TricksController < ApplicationController
 
     # (style-combo).count == style.count - combo.count
     # this hits the database twice. is there a better way?
-    @combos_you_can_do.reject! { |c| ((@trick_list-c.tricks.map(&:id)).count != (@trick_list.count - c.tricks.map(&:id).count)) }
+    @combos_you_can_do.reject! { |c| ((@trick_list-c.tricks.map(&:id)).count != (@trick_list.count - c.tricks.uniq.map(&:id).count)) }
   end
 
 end
