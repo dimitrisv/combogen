cb9df7c9911d5822116c1cf05580648a877e41bd
DimitrisVainas <vainas.dim@gmail.com>
2014-04-12 21:17:03 +0300

    Add uid to vieos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,17 +1,26 @@
 class Video < ActiveRecord::Base
+  
+  # NOTE: Currently supports YouTube video only!
+
   belongs_to :tricker
   belongs_to :trick
   belongs_to :combo
-  attr_accessible :url, :tricker_id, :combo_id, :trick_id, :start_time, :end_time
+  attr_accessible :url, :uid, :tricker_id, :combo_id, :trick_id, :start_time, :end_time
 
-  # Currently assumes youtube video
-  def get_video_id
+  before_save :extract_uid
+
+
+
+  private
+
+  def extract_uid   
     if url[/youtu\.be\/([^\?]*)/]
-      youtube_id = $1
+      self.uid = $1
     else
-      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      # Regex from
+      # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
       url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-      youtube_id = $5
+      self.uid = $5
     end
     # later I can insert this directly as a data attribute
     # %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
