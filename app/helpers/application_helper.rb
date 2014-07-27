ca1748bd2d78403a2458c000be4fba63b24f261b
DimitrisVainas <vainas.dim@gmail.com>
2014-07-27 16:02:39 +0300

    Fix edit bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
 
-  def current_tricker_can_edit_trick
-    current_tricker.try(:admin?) || (!@trick.tricker.nil? && (@trick.tricker.id.equal? current_tricker.id))
+  def current_tricker_can_edit_trick(trick)
+    current_tricker.try(:admin?) || (!trick.tricker.nil? && (trick.tricker.id.equal? current_tricker.id))
   end
 
-  def current_tricker_can_edit_combo
-    current_tricker.try(:admin?) || (@combo.tricker.id.equal? current_tricker.id)
+  def current_tricker_can_edit_combo(combo)
+    current_tricker.try(:admin?) || (combo.tricker.id.equal? current_tricker.id)
   end
 
-  def current_tricker_can_edit_style
-    current_tricker.try(:admin?) || (@tricking_style.tricker_id.equal? current_tricker.id)
+  def current_tricker_can_edit_style(tricking_style)
+    current_tricker.try(:admin?) || (tricking_style.tricker_id.equal? current_tricker.id)
   end
 
   def get_trick_types
