ef3cd4b03c991823bed8c55597f0016f7cc5a7bf
DimitrisVainas <vainas.dim@gmail.com>
2014-08-06 00:00:43 +0300

    Enable devise rememberable module

--- app/models/tricker.rb
+++ app/models/tricker.rb
@@ -5,20 +5,12 @@ class Tricker < ActiveRecord::Base
   has_many :combos, :dependent => :delete_all
   has_many :videos, :dependent => :delete_all
   
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, #:confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name, :sampler, :youtube, :facebook, :admin
-  # attr_accessible :title, :body
 
   validates :name, :presence => true, :uniqueness => true, length: { minimum: 3, maximum: 20 }
 
-  protected
-  def confirmation_required?
-    false
-  end
 end
